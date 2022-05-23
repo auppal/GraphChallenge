359817379995c9aab81c83b5a972f774daed31d3
Ahsen Uppal <auppal@gwu.edu>
2022-05-22 21:24:51 -0400

    Work in progress.

--- StochasticBlockPartition/code/python/compressed_array.c
+++ StochasticBlockPartition/code/python/compressed_array.c
@@ -18,8 +18,7 @@
 struct hash {
   uint64_t *keys;
   uint64_t *vals;
-  size_t l; /* allocated size of each table */
-  size_t m; /* m <= l */
+  size_t w; /* width of each hash table */
   size_t n; /* the number of tables */
 };
 
@@ -99,7 +98,9 @@ struct compressed_array *compressed_array_create(size_t n_nodes, size_t max_degr
     return NULL;
   }
 
-  size_t table_size = (n_nodes * max_degree * 1.43);
+  max_degree = n_nodes;
+  size_t table_width = max_degree * 1.43;
+  size_t table_size = n_nodes * table_width;
   size_t buf_size = 4 * table_size * sizeof(uint64_t);
 
   x->shm_path = shm_path;
@@ -116,19 +117,147 @@ struct compressed_array *compressed_array_create(size_t n_nodes, size_t max_degr
 
   x->rows.keys = (uint64_t *) x->buf + 0 * table_size;
   x->rows.vals = (uint64_t *) x->buf + 1 * table_size;
-  x->rows.l = max_degree;
-  x->rows.m = max_degree;
+  x->rows.w = table_width;
   x->rows.n = n_nodes;
   
   x->cols.keys = (uint64_t *) x->buf + 2 * table_size;
   x->cols.vals = (uint64_t *) x->buf + 3 * table_size;
-  x->cols.l = max_degree;
-  x->cols.m = max_degree;
+  x->cols.w = table_width;
   x->cols.n = n_nodes;
 
+#if 0
+  memset(x->buf, 0x00, buf_size);
+#else
   memset(x->buf, 0xff, buf_size);
+#endif
   return x;
 }
+
+int compressed_get_single(struct compressed_array *x, uint64_t i, uint64_t j, uint64_t *val)
+{
+#if 0
+  *val = *(x->rows.keys + (i * x->rows.n) + j);
+  return 0;
+#else
+  uint64_t *base, *loc;
+  long k, offset, L;
+  uint64_t hj = hash(j);  
+
+  /* get from row[i][j] */
+  L = x->rows.w;
+  offset = hj % L;
+  base = (x->rows.keys + i * L);
+
+  for (k=0; k<L; k++) {
+    long idx = (k + offset) % L;
+    loc = base + idx;
+
+    if (*loc & EMPTY_FLAG) {
+      break;
+    }
+    else if (*loc == j) {
+      *val = *(x->rows.vals + i * L + idx);
+      return 0;
+    }
+  }
+#endif
+  return -1;
+}
+
+int compressed_set_single(struct compressed_array *x, uint64_t i, uint64_t j, uint64_t val)
+{
+#if 0
+  *(x->rows.keys + (i * x->rows.n) + j) = val;
+#else
+  uint64_t *base, *loc;
+  long k, offset, L;
+  uint64_t hi = hash(i);
+  uint64_t hj = hash(j);
+
+  //fprintf(stderr, "Set item %ld %ld = %ld\n", i, j, val);
+  
+  /* set row[i][j] = val */
+  L = x->rows.w;
+  offset = hj % L;
+  base = (x->rows.keys + i * L);
+
+  for (k=0; k<L; k++) {
+    long idx = (k + offset) % L;
+    loc = base + idx;
+
+    if (*loc & EMPTY_FLAG) {
+      *loc = j;
+      *loc &= ~EMPTY_FLAG;
+      *(x->rows.vals + i * L + idx) = val;
+      //fprintf(stderr, "Setting at row %ld idx %ld to %ld\n", i, idx, val);
+      break;
+    }
+  }
+
+  if (k == L) {
+    return -1;
+  }
+
+  /* set col[j][i] = val */
+  L = x->cols.w;
+  offset = hi % L;
+  base = (x->cols.keys + j * L);
+
+  for (k=0; k<L; k++) {
+    long idx = (k + offset) % L;    
+    loc = base + idx;
+
+    if (*loc & EMPTY_FLAG) {
+      *loc = i;
+      *loc &= ~EMPTY_FLAG;
+      *(x->cols.vals + j * L + idx) = val;
+      break;
+    }
+  }
+
+  if (k == L) {
+    return -1;
+  }
+
+  /* set col[j][i] = val */
+  return 0;
+#endif
+}
+
+int compressed_take(struct compressed_array *x, long idx, long axis, uint64_t *keys, uint64_t *vals, long *p_cnt)
+{
+  uint64_t *p, *q;
+  long k, L;
+  long cnt = 0;
+
+  //  fprintf(stderr, "compressed take idx %ld axis %ld\n", idx, axis);
+  
+  if (axis == 0) {
+      L = x->rows.w;    
+      p = (x->rows.keys + idx * L);
+      q = (x->rows.vals + idx * L);
+
+  }
+  else { /* axis == 0 */
+      L = x->cols.w;
+      p = (x->cols.keys + idx * L);
+      q = (x->cols.vals + idx * L);      
+  }
+
+  for (k=0; k<L; k++) {
+    if ((*p & EMPTY_FLAG) == 0) {
+      *keys++ = *p;
+      *vals++ = *q;
+      cnt++;
+    }
+    p++;
+    q++;
+  }
+
+  *p_cnt = cnt;
+  return 0;
+}
+
 
 void compressed_array_destroy(struct compressed_array *p)
 {
@@ -147,7 +276,6 @@ void compressed_array_destroy(struct compressed_array *p)
 static void compressed_destroy(PyObject *obj)
 {
   void *x = PyCapsule_GetPointer(obj, "compressed_array");
-  fprintf(stderr, "Destroying %p\n", x);
   compressed_array_destroy(x);
 }
 
@@ -164,14 +292,97 @@ static PyObject* compressed_create(PyObject *self, PyObject *args)
   struct compressed_array *x = compressed_array_create(n_nodes, max_degree);
 
   uint64_t flag = EMPTY_FLAG;
-  fprintf(stderr, "flag is 0x%lx\n", flag);
   fprintf(stderr, "Created tables for n_nodes %ld with degree %ld = %p\n", n_nodes, max_degree, x);
 
   ret = PyCapsule_New(x, "compressed_array", compressed_destroy);
-  fprintf(stderr, "New object ptr is %p\n", ret);
   return ret;
   
 }
+
+static PyObject* setitem(PyObject *self, PyObject *args)
+{
+  PyObject *obj;
+  long i, j, val;
+
+  if (!PyArg_ParseTuple(args, "Olll", &obj, &i, &j, &val))
+    return NULL;
+  
+  struct compressed_array *x = PyCapsule_GetPointer(obj, "compressed_array");
+  compressed_set_single(x, i, j, val);
+  Py_RETURN_NONE;
+}
+
+static PyObject* getitem(PyObject *self, PyObject *args)
+{
+  PyObject *obj;
+  long i, j;
+  unsigned long val;
+
+  if (!PyArg_ParseTuple(args, "Oll", &obj, &i, &j))
+    return NULL;
+  
+  struct compressed_array *x = PyCapsule_GetPointer(obj, "compressed_array");
+
+  if (compressed_get_single(x, i, j, &val) < 0) {
+    val = 0;
+  }
+
+  PyObject *ret = Py_BuildValue("k", val);
+  return ret;
+}
+
+static PyObject* take(PyObject *self, PyObject *args)
+{
+  PyObject *obj;
+  long i, axis;
+
+  if (!PyArg_ParseTuple(args, "Oll", &obj, &i, &axis))
+    return NULL;
+
+  struct compressed_array *x = PyCapsule_GetPointer(obj, "compressed_array");
+
+#if 1
+  long cnt, cnt_max = x->rows.w;
+
+  uint64_t *keys = malloc(cnt_max * sizeof(uint64_t));
+  uint64_t *vals = malloc(cnt_max * sizeof(uint64_t));
+
+  compressed_take(x, i, axis, keys, vals, &cnt);  
+
+  npy_intp dims[] = {cnt};
+
+  PyObject *keys_obj = PyArray_SimpleNewFromData(1, dims, NPY_LONG, keys);
+  PyObject *vals_obj = PyArray_SimpleNewFromData(1, dims, NPY_LONG, vals);
+
+  PyArray_ENABLEFLAGS((PyArrayObject*) keys_obj, NPY_ARRAY_OWNDATA);
+  PyArray_ENABLEFLAGS((PyArrayObject*) vals_obj, NPY_ARRAY_OWNDATA);  
+  
+#if 0
+  for (i=0; i<x->rows.w; i++) {
+    keys[i] = i;
+  }
+#endif
+#else  
+  int N = 22;
+  uint64_t *kk = malloc(N * sizeof(uint64_t));
+  uint64_t *vv = malloc(N * sizeof(uint64_t));
+
+  for (i=0; i<N; i++) {
+    kk[i] = i;
+    vv[i] = i;
+  }
+
+  npy_intp dims[] = {N};
+  PyObject *keys_obj = PyArray_SimpleNewFromData(1, dims, NPY_LONG, kk);
+  PyObject *vals_obj = PyArray_SimpleNewFromData(1, dims, NPY_LONG, vv);
+  PyArray_ENABLEFLAGS((PyArrayObject*) keys_obj, NPY_ARRAY_OWNDATA);
+  PyArray_ENABLEFLAGS((PyArrayObject*) vals_obj, NPY_ARRAY_OWNDATA);  
+  
+#endif
+  PyObject *ret = Py_BuildValue("OO", keys_obj, vals_obj);
+  return ret;
+}
+
 
 static PyObject* get_magic(PyObject *self, PyObject *args)
 {
@@ -199,8 +410,7 @@ static PyObject* set_magic(PyObject *self, PyObject *args)
   fprintf(stderr, "Set magic to 0x%lx\n", val);
   *x->magic = val;
 
-  PyObject *ret = Py_BuildValue("l", 0);
-  return ret;
+  Py_RETURN_NONE;
 }
 
 static PyMethodDef compressed_array_methods[] =
@@ -217,6 +427,25 @@ static PyMethodDef compressed_array_methods[] =
     METH_VARARGS,
     "Get magic value."
    },
+   {
+    "setitem",
+    setitem,
+    METH_VARARGS,
+    "Set an item."
+   },
+   {
+    "getitem",
+    getitem,
+    METH_VARARGS,
+    "Get an item."
+   },
+   {
+    "take",
+    take,
+    METH_VARARGS,
+    "Take items along an axis."
+   },
+
    {
     "set_magic",
     set_magic,
